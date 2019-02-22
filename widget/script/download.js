439dd80ec388fee8eb51d7b19339869a5ce3a098
goodhlsh <35991667+goodhlsh@users.noreply.github.com>
2019-02-22 12:31:26 +0800

    图

--- widget/script/download.js
+++ widget/script/download.js
@@ -23,13 +23,10 @@ function fnLoadOnlyImage(url_,callback) {
                 if (ret.state == 1) {
                   callback(ret.savePath,ret,err);
                 }
-                else {
-
-                }
+                else {                }
             });
         }
     });
-
 }
 function fnLoadImage(imgObj_, url_) {
   if (url_==''||url_==null) {
@@ -40,15 +37,15 @@ function fnLoadImage(imgObj_, url_) {
     var imgSha1 = sha1(url_);
     var fs = api.require('fs');
     fs.exist({
-        path: imgDir + imgSha1 + expname
+        path: imgDir + '/'+imgSha1 + expname
     }, function(ret, err) {
         if (ret.exist) {
-            $api.attr(imgObj_, "src", imgDir + imgSha1 + expname);
+            $api.attr(imgObj_, "src", imgDir + '/'+imgSha1 + expname);
         } else {
             api.download({
                 //url: 'http://154.8.159.50:1693'+url_,
                 url: 'http://132.232.190.152:8088'+url_,
-                savePath: imgDir + imgSha1 + expname,
+                savePath: imgDir + '/'+imgSha1 + expname,
                 report: true,
                 cache: true,
                 allowResume: true
